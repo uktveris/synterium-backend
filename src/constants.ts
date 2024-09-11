3ac37c6f0c3ee97e428a757896aae23e0755bac2
uktveris <marius.uktveris@gmail.com>
2024-09-11 13:54:32 +0300

    simple endpoint

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+const users: { email: string; password: string }[] = [
+  { email: "john@gmail.com", password: "123" },
+  { email: "dave@gmail.com", password: "000" },
+  { email: "sarah@gmail.com", password: "111" },
+  { email: "mike@gmail.com", password: "098765" },
+  { email: "matthew@gmail.com", password: "mypassword" },
+  { email: "julia@gmail.com", password: "strongafpassword123" },
+];
+
+export { users };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,35 @@
 import { config, configDotenv } from "dotenv";
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { users } from "./constants";
+import bodyParser from "body-parser";
 
 configDotenv();
 
 const PORT = process.env.PORT;
+const SECRET = process.env.SESSION_SECRET || "default-secret";
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 * 60 * 3, secure: false },
+  }),
+);
 
 const messages = [
   { owner: "first", message: "my first message!" },
@@ -27,4 +48,23 @@ app.get("/", (req, res) => {
 
 app.get("/api/test", (req, res) => {
   res.status(200).send(messages);
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  console.log("received email: " + email);
+  console.log("received password: " + password);
+  const user = users.find((u) => u.email === email);
+  if (!user) {
+    return res.status(400).send({ msg: "no user found" });
+  } else if (user.password !== password) {
+    return res.status(400).send({ msg: "incorrect password" });
+  }
+  console.log("hello");
+  req.session.user = { email };
+  console.log("session info:");
+  console.log(req.session);
+  console.log(req.session.id);
+  return res.status(201).send({ msg: "successfully logged in!" });
 });
