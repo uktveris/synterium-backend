19cc4351a843004a3bb8376d6b1a12b0a6286866
uktveris <marius.uktveris@gmail.com>
2024-09-12 19:13:40 +0300

    added more structure

---
+++ src/middleware/eventLogger.ts
@@ -0,0 +1,33 @@
+// import { EventEmitter } from "events";
+import { format } from "date-fns";
+import { randomUUID } from "crypto";
+import fspromises from "fs/promises";
+import path from "path";
+import fs from "fs";
+
+// class CustomEmitter extends EventEmitter {}
+
+// const emitter = new CustomEmitter();
+
+// emitter.on("log", (message) => {});
+
+function logEvent(message: string, filename: string) {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const log = `${dateTime}\t${randomUUID()}\t${message}\n`;
+  if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+    fspromises
+      .mkdir(path.join(__dirname, "..", "logs"))
+      .catch((err) => console.log((err as Error).message));
+  }
+  fspromises
+    .appendFile(path.join(__dirname, "..", "logs", filename), log)
+    .catch((err) => console.log((err as Error).message));
+}
+
+function logger(req, res, next) {
+  const msg = `${req.method}\t${req.url}`;
+  logEvent(msg, "reqlog.txt");
+  next();
+}
+
+export { logger };
--- src/server.ts
+++ src/server.ts
@@ -2,34 +2,25 @@ import { config, configDotenv } from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import session from "express-session";
 import { users } from "./constants";
 import bodyParser from "body-parser";
+import { logger } from "./middleware/eventLogger";
+import { corsOptions } from "./config/corsOptions";
+import jwt from "jsonwebtoken";
 
 configDotenv();
 
 const PORT = process.env.PORT;
 const SECRET = process.env.SESSION_SECRET || "default-secret";
-const corsOptions = {
-  origin: "http://localhost:5173",
-  optionsSuccessStatus: 200,
-};
 
 const app = express();
 
+app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  session({
-    secret: SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60000 * 60 * 3, secure: false },
-  }),
-);
 
 const messages = [
   { owner: "first", message: "my first message!" },
@@ -37,10 +28,6 @@ const messages = [
   { owner: "pilot", message: "hello there" },
   { owner: "mate", message: "the las one..." },
 ];
-
-app.listen(PORT, () => {
-  console.log("the app is running on port = " + PORT);
-});
 
 app.get("/", (req, res) => {
   res.status(200).send("hello from the server..");
@@ -61,10 +48,22 @@ app.post("/login", (req, res) => {
   } else if (user.password !== password) {
     return res.status(400).send({ msg: "incorrect password" });
   }
+  const accessToken = jwt.sign(
+    { email: user.email },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    { expiresIn: "5m" },
+  );
+  const refreshToken = jwt.sign(
+    { email: user.email },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    { expiresIn: "1d" },
+  );
   console.log("hello");
-  req.session.user = { email };
-  console.log("session info:");
-  console.log(req.session);
-  console.log(req.session.id);
-  return res.status(201).send({ msg: "successfully logged in!" });
+  // return res.status(201).send({ msg: "successfully logged in!" });
+  res.cookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60000 });
+  return res.json({ accessToken });
+});
+
+app.listen(PORT, () => {
+  console.log("the app is running on port = " + PORT);
 });
